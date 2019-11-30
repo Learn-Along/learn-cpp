bcdabcf0058dca844d7cb6e91105608ed3099479
Martin Ahindura <martinahindura@gmail.com>
2019-11-30 16:42:47 +0300

    Add constructor

--- w3schools-tutorial/classes.cpp
+++ w3schools-tutorial/classes.cpp
@@ -11,6 +11,14 @@ public:       // Access specifier
     int year;
     string class_teacher_name;
 
+    // Constructors have to be public
+    CustomClass(int size_of_class, int currentYear, string current_class_teacher_name)
+    {
+        size = size_of_class;
+        year = currentYear;
+        class_teacher_name = current_class_teacher_name;
+    }
+
     // We can add methods which can even have default parameters
     void increase_size(int additional_size = 5)
     {
@@ -33,15 +41,23 @@ int main()
 {
     // instantiation is as though one is decalring a new variable
     // of type of the class
-    CustomClass customObject;
+    CustomClass customObject(20, 1999, "Mr. Musinguzi");
+    cout << "customObject.size: " << customObject.size << endl;
+    cout << "customObject.class_teacher_name: "
+         << customObject.class_teacher_name << endl;
+    cout << "customObject.year: " << customObject.year << endl
+         << endl;
+
     customObject.class_teacher_name = "Tr. Aggrey";
     customObject.size = 90;
     customObject.year = 2019;
 
-    cout << "customObject.size: " << customObject.size << endl;
-    cout << "customObject.class_teacher_name: "
+    // properties can be reset
+    cout << "customObject.size [changed]: " << customObject.size << endl;
+    cout << "customObject.class_teacher_name [changed]: "
          << customObject.class_teacher_name << endl;
-    cout << "customObject.year: " << customObject.year << endl;
+    cout << "customObject.year [changed]: " << customObject.year << endl
+         << endl;
 
     // Increase size
     customObject.increase_size(7);
